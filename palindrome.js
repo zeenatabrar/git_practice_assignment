5e35eefde693d0ff2642572949e7c8e3bdab6698
Zeenat Abrar <zeenatabrar50@gmail.com>
2023-02-09 04:46:25 +0530

    first commit

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str="zeenat";
+let a="";
+   for(let i=str.length-1;i>=0;i--){
+        a=a+str[i];
+    }
+    if(a==str){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
---
+++ prime.js
@@ -0,0 +1,26 @@
+let num=13;
+let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i===0)
+        count++;
+        
+    }
+    if(count==2){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
+}
+let count=0;
+for(let i=1;i<=num;i++){
+    if(num%i===0){
+        count++;
+    }
+}
+if(count==1){
+    console.log("yes")
+}
+else{
+  console.log("no")
+}
